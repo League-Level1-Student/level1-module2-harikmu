d05ac8ca5110aadadb3d4b4dd675ccc0b4c56572
league <league@wts-im-17.attlocal.net>
2018-05-18 17:01:02 -0700

    Popcorn Minion

--- src/MinionTest.java
+++ src/MinionTest.java
@@ -48,6 +48,7 @@ public class MinionTest {
 		assertEquals("Napoleon", stuart.getMaster());
 	}
 
+	
 }
 
 
---
+++ src/PopcornMaker.java
@@ -0,0 +1,7 @@
+public class PopcornMaker {
+	public static void main(String[] args){
+	}
+	Popcorn() {
+		System.out.println("Microwave says: a Popcorn has been made");
+	}
+}
